4b839f3b66837a7f96e652abdca708227fc2ac50
Jusabe Guedes <jusabe.09@gmail.com>
2019-08-12 16:01:16 +0200

    add install

---
+++ install.sh
@@ -0,0 +1,10 @@
+# install xcode tool belt
+xcode-select --install
+
+# install brew
+ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
+
+# install relevant apps
+brew cask install google-chrome
+brew cask install slack
+brew cask install spectacle
