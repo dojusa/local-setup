d69ccc49618b7c2731448d9936658e4b01696bb9
Jusabe Guedes <jusabe.09@gmail.com>
2019-08-12 17:34:12 +0200

    update install

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
-# install xcode tool belt
+#!/bin/bash
+set -euo pipefail
+
+echo "instaling xcode tool belt"
 xcode-select --install
 
-# install brew
+echo "installing brew"
 ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
 
-# install relevant apps
+echo "installing relevant apps"
 brew cask install google-chrome
 brew cask install slack
 brew cask install spectacle
+brew cask install visual-studio-code
+brew cask install spotify
+
+echo "installing development apps"
+brew cask install iterm2
+brew install zsh
