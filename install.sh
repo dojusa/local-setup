70d853e94ac6400a504af4807eca14fd912ac8d1
Jusabe Guedes <jusabe.09@gmail.com>
2019-08-13 12:23:40 +0200

    update install

--- install.sh
+++ install.sh
@@ -15,9 +15,33 @@ brew cask install visual-studio-code
 brew cask install spotify
 
 echo "installing development apps"
+brew cask install adoptopenjdk
+brew cask install docker
 brew cask install iterm2
 brew install zsh
 
 echo "installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 upgrade_oh_my_zsh
+
+echo "installing asdf"
+brew install asdf
+echo -e "\n. $(brew --prefix asdf)/asdf.sh" >> ~/.zshrc
+echo -e "\n. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash" >> ~/.zshrc
+echo '\nexport ASDFROOT=$HOME/.asdf' >> ~/.zshrc
+echo '\nexport ASDFINSTALLS=$HOME/.asdf/installs' >> ~/.zshrc
+source ~/.zshrc
+
+echo "installing programming languages"
+asdf plugin-add golang
+asdf install golang 1.12
+asdf global golang 1.12
+
+mkdir -p ~/workspace/go
+echo "\nGOV=$(asdf current golang | awk '{print $1}')" >> ~/.zshrc
+# echo '\nexport GOROOT=$ASDFINSTALLS/golang/$GOV/go' >> ~/.zshrc
+echo '\nexport GOPATH=$HOME/workspace/go' >> ~/.zshrc
+echo '\nexport PATH=$PATH:$GOPATH/bin' >> ~/.zshrc
+
+asdf plugin-add erlang
+asdf plugin-add elixir
