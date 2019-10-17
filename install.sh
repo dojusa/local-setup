b8d2e017182ff9c6c77966ed85ddac4a0765fc33
Jusabe Guedes <jusabe.09@gmail.com>
2019-10-17 15:45:56 +0200

    add diff-so-fancy

--- install.sh
+++ install.sh
@@ -82,3 +82,22 @@ echo '\n**/.elixir_ls/' >> ~/.gitignore_global
 
 echo "configuring global pull.rebase true"
 git config --global pull.rebase true
+
+echo "configuring diff-so-fancy"
+brew install diff-so-fancy
+
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --bool --global diff-so-fancy.markEmptyLines false
+
+git config --global color.ui true
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "11"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
